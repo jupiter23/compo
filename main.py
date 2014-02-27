df14216bdb5fda6d157c02348fb767cd7b0b93f1
Jupiter <jm@jupiterweb.ca>
2014-02-27 11:05:05 -0500

    strum test update

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ strum = Strummer(inst=env)
 
 def test_strum():
   global count
-  if (count%3==0):
-    beat_time.setValue(.5)
-  else:
-    beat_time.setValue(.75)
+#  if (count%3==0):
+#    beat_time.setValue(.5)
+#  else:
+#    beat_time.setValue(.75)
 
   # Set a random value for the metro time.
   beat_time.setValue(randint(4,6)*.1)
