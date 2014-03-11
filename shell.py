a44d26c9d3f824717ba3653a6885e9421228e489
Jupiter <jm@jupiterweb.ca>
2014-03-10 20:49:21 -0400

    risset exp

--- shell.py
+++ shell.py
@@ -17,16 +17,15 @@ class ShellManager():
   def __init__(self, shell):
     self._sigs = [Sig(0) for i in range(shell.getNum())]
     self.setShell(shell)    
-    self._shell = shell
   
   # Play each envelope or just specified envelopes
   def play(self, pos='all'):
     if (pos=='all'):
-      for s in self._envs:
-        s.play()
+      for t in self._trigs:
+        t.play()
     else:
       for i in pos:
-        self._envs[i].play()
+        self._trigs[i].play()
  
   # Update the value of each sig
   def setShell(self, shell):
@@ -34,6 +33,7 @@ class ShellManager():
       self._sigs[i].setValue(env)
     self._shell = shell
     self._envs = shell.getEnvs()
+    self._trigs = shell.getTrigs()
 
   # Update the dur for each enveloppe    
   def setShellDur(self, dur):
@@ -73,7 +73,10 @@ class Shell():
        
   def getEnvs(self):
     return self._envs
-  
+    
+  def getTrigs(self):
+    return self._trigs  
+    
   def getNum(self):
     return self._num
 
@@ -82,7 +85,7 @@ class Shell():
 
 class EnvWrap():
   """
-  Provides an envelope player wrapper.
+  Provides an envelope player wrapper.  Used by instruments that provide their own envelopes.
   :Args:
     wrapped: List of Pyo tables that need to be red on a Trig.
   """
@@ -96,20 +99,75 @@ class EnvWrap():
     if pos == 'all':
       for t in self._trigs:
         t.play()
-    
+    else:
+      for i in pos:
+        self._envs[i].play()
+
+class ShellHann(Shell):
+  """
+  Provide a number of Hann tables to be read as sound envelopes.
+  
+  :Args:
+    dur: time duration for table reading by TrigEnv
+    num: number of Tables to generate
+    dur_fact: multiplier for the dur parameter
+    items_dur_fact: individual dur multiplier for each envoloppe reader
+    items_start_del: Listdelay on reading of each table reader
+    mul: List of amplitude control factor for each envelope.  If not provided mul will be calculated to 1/num
+  """
+  def __init__(self, dur, num=1, dur_fact=1, items_dur_fact=[], items_start_del=[], mul=[]):
+    Shell.__init__(self, num=num, dur=dur, dur_fact=dur_fact)
+    self._num_list = range(0, num)
+    if not mul:
+      self._mul = [1./num for x in self._num_list]
+    else:
+      self._mul = [0 for x in self._num_list]
+      for i, mul_i in enumerate(mul):
+        self._mul[i] = mul[i%len(mul)]      
+        
+    self._tables = [HannTable() for x in self._num_list]
+    self._trigs = [Trig() for x in self._num_list]
+    self._envs = [TrigEnv(self._trigs[i], table=self._tables[i], mul=self._mul[i]) for i in self._num_list]
+    self.setShellDur(dur, dur_fact, items_dur_fact)
  
+  # Repetitive calls do this method that don't want to change one of the parameters should pass None.
+  def setDurValues(self, dur=None, dur_fact=None, items_dur_fact=None):
+    if dur != None:
+      self._dur_orig = dur
+    if dur_fact != None:
+      self._dur_fact = dur_fact
+    if items_dur_fact != None:
+      self._items_dur_fact = items_dur_fact
+
+    dur = self._dur_orig * self._dur_fact
+
+    # Each envelope has the same dur
+    if not items_dur_fact:
+      print 'fnord'
+      self._dur = [dur for i in self._num_list]
+    # Apply a factor specific to each envelope
+    else:
+      self._dur = [i for i in self._num_list]
+      for i, item in enumerate(items_dur_fact):
+        self._dur[i] = dur * items_dur_fact[i%len(items_dur_fact)]
+    
+  def setShellDur(self, dur=None, dur_fact=None, items_dur_fact=None):
+    if items_dur_fact == None:
+      items_dur_fact = self._items_dur_fact
+    self.setDurValues(dur=dur, dur_fact=dur_fact, items_dur_fact=items_dur_fact)
+    for i, env in enumerate(self._envs):
+      env.setDur(self._dur[i])
+
 class ShellAdsr(Shell):
   """
+  # @TODO map self._trigs to Adsr objects play method 
+  
   Wraps a number of Adsr pyo objects.
   
   :Args:
-
-    num: number of Adrs objects
-
     dur: Adsr dur
-
+    num: number of Adrs objects
     dur_fact: multiplier for the Adsr dur parameter
-
   """
   def __init__(self, dur, num=1, dur_fact=1):
     Shell.__init__(self, num=num, dur=dur, dur_fact=dur_fact)
@@ -126,3 +184,44 @@ class ShellAdsr(Shell):
       env.setSustain(.6)
       env.setRelease(dur*.1)
       env.setDur(dur)
+
+if __name__ == '__main__':
+  from pitches import *
+  from random import randint
+  from random import choice
+
+  s = Server().boot()
+  p = Pitches(degrees=[0,1,2,3,4,5,6,7], tuning='harmonic')
+#   p = Pitches(degrees=[0,4,7,12], tuning='just')
+  beat_time = Sig(.15)
+  shells = [
+    ShellHann(dur=beat_time.value, num=8, dur_fact=.5, items_dur_fact=[.5, .45, .4, .35, .3, .25, .2, .15]),
+    ShellHann(dur=beat_time.value, num=8, dur_fact=.9, items_dur_fact=[.4, .05, .04, .035, .03, .025, .02, .05]),
+    ShellHann(dur=beat_time.value, num=8, dur_fact=.6, items_dur_fact=[.02, .05, .03, .3, .03, .4, .2, .25])
+  ]
+  m = Metro(beat_time, poly=1).play()
+  
+  # Generate the envelope manager
+  env = ShellManager(shell=shells[0])
+  sin = Sine(freq=p.getPitches(), mul=env.getShell())
+  mix = Pan(sin).out()
+  
+  count = 0
+  def play_me():
+    global count
+    if count%3==0:
+      root = 100*randint(2,6)
+      p.setRoot(root)
+      
+    bt = choice([.15, .3, .15, .15, .6, .15, .3])
+    beat_time.setValue(bt)
+    shell_num = randint(0,2)
+    env.setShell(shells[shell_num])
+    env.setShellDur(beat_time.value)
+ 
+    env.play()
+    count += 1
+  
+  tf = TrigFunc(m, play_me)
+  s.gui(locals())  
+    
--- synth.py
+++ synth.py
@@ -4,17 +4,16 @@ from shell import *
 class RissetSnare(Shell):
   """
   Implementation of Risset Snare as described in Computer Music, Dodge, 1997.
-  This class inherits the Shell class and manages its envelopes.
-  
+  This class inherits the Shell class and manages its envelopes and can't be used with a ShellManager.
   """
-  def __init__(self, pitches, dur, dur_fact=1):
+  def __init__(self, pitches, dur, dur_fact=1, noise_freq=500, randi_freq=500):
     Shell.__init__(self, dur, num=1, dur_fact=1)
-    self.f1 = ExpTable(list=[(0, 0.0), (32, 1.0), (8192, 0.0)], exp=10)
-    self.f2 = ExpTable(list=[(0, 0.0), (32, 1.0), (8192, 0.0)], exp=15)
+    self.f1 = ExpTable(list=[(0, 0.0), (4, 1.0), (8192, 0.0)], exp=10)
+    self.f2 = ExpTable(list=[(0, 0.0), (4, 1.0), (8192, 0.0)], exp=15)
     self.t1 = TrigEnv(Trig(), table=self.f1, dur=dur, mul=.09)
-    self.t2 = TrigEnv(Trig(), table=self.f2, dur=dur, mul=.07)
-    self.randi = Randi(freq=400, mul=self.t2)
-    self.noise = Sine(freq=500, mul=self.randi)
+    self.t2 = TrigEnv(Trig(), table=self.f2, dur=dur, mul=.05)
+    self.randi = Randi(freq=randi_freq, mul=self.t2)
+    self.noise = Sine(freq=noise_freq, mul=self.randi)
    
     # Wrap the envelopes
     self._envs = EnvWrap([self.t1, self.t2])
@@ -25,7 +24,7 @@ class RissetSnare(Shell):
     self.inharms = [0 for x in range(0, self.num)]
     self.funds = [0 for x in range(0, self.num)]
     for i, pitch in enumerate(self.pitches):
-      self.inharms[i] = Sine(freq=[pitch, pitch*1.6, pitch*2.2, pitch*2.3], mul=[.5, .75, 1., .75]) * .006 * self.t2 
+      self.inharms[i] = Sine(freq=[pitch, pitch*1.6, pitch*2.2, pitch*2.3], mul=[.5, .75, 1., .75]) * .06 * self.t2 
       self.funds[i] = Sine(freq=pitch, mul=self.t1) 
     
   def setShellDur(self, dur):
@@ -33,7 +32,8 @@ class RissetSnare(Shell):
     self.t2.setDur(dur)
 
   def out(self):
-    return Mix([self.noise + self.inharms[i] + self.funds[i] for i in range(0, self.num)], mul=1./self.num).out()
+    voices = self.num if self.num > 1 else 2 
+    return Mix([self.noise + self.inharms[i] + self.funds[i] for i in range(0, self.num)], mul=1./self.num, voices=voices).out()
 
 class Ring426():
   def __init__(self):
@@ -45,38 +45,52 @@ class Ring426():
     
 if __name__ == '__main__':
   from pitches import Pitches
+  from random import randint
+  from random import choice
+
   s = Server().boot()
+  
 #  p = Pitches(degrees=[0, 7, 10, 12], root=400)
-  p = Pitches(degrees=[0], root=300)
-  snr = RissetSnare(pitches=p, dur=2)
-  out = Mix(snr.out(), voices=2).out()
+  p = Pitches(degrees=[0], root=200)
+  snr = RissetSnare(pitches=p, dur=5, noise_freq=400, randi_freq=500)
+  mul_sig = Sig(.8)
+  out = snr.out()
+#   out.setMul(mul_sig)
   env = snr.getEnvs()
-  m = Metro(time=3).play()
+  beat = Sig(5)
+  m = Metro(beat).play()
+  count = 0
   
   def play_me():
+    global count
+#     root = 200*choice([0.95,0.97,1,1.02,1.05,1.06])
+#     p.setRoot(root)
+    if count%3 == 0:
+      beat.setValue(5)
+#       mul_sig.setValue(.6)
+    else:
+      beat.setValue(3)
+#       mul_sig.setValue(.3)
+
     env.play()
+    count += 1
     
   tf = TrigFunc(m, play_me)
+  s.gui(locals())
  
   if False:
     dur = 0.5
     freq = 400
     f1 = ExpTable(list=[(0, 0.0), (32, 1.0), (8192, 0.0)], exp=10)
     f2 = ExpTable(list=[(0, 0.0), (32, 1.0), (8192, 0.0)], exp=15)
-  #  osc_f1 = Osc(table=f1, freq=0.5, mul=.25)
-  #  osc_f2 = Osc(table=f2, freq=0.5, mul=.3) 
     m = Metro(time=2).play()
     t1 = TrigEnv(m, table=f1, dur=1, mul=.25)
     t2 = TrigEnv(m, table=f2, dur=1, mul=.3)
     
-  #  t1 = OscTrig(table=f1, trig=m, freq=2, mul=.25)
-  #  t2 = OscTrig(table=f2, trig=m, freq=2, mul=.3)
     randi = Randi(freq=400, mul=t1)
     noise = Sine(freq=500, mul=randi)
     inharm = Sine(freq=[freq, freq*1.6, freq*2.2, freq*2.3], mul=[.5, .75, 1., .75]) *.1 * t2 
     fund = Sine(freq=freq, mul=t1)
     mix = Mix(noise+fund+inharm, voices=2, mul=1).out()
   
-#   spec = Spectrum(sine)
-  s.gui(locals())
   
